cba02a20d2901e265d2dd282730a360933ec3660
vinodm1986 <vinodm1986@gmail.com>
2016-04-07 12:11:11 +0530

    Adding tracer

--- controllers/member-controller.js
+++ controllers/member-controller.js
@@ -8,7 +8,7 @@ var logger = log4js.getLogger("app");
 //Send success response
 var sendJsonResponse = function (res, json) {
     try {
-        logger.trace('starting sendJsonResponse');
+        logger.trace('entering sendJsonResponse');
         res.json(json);
     } catch (err) {
         logger.error('rendering json response failed with error:' + err);
@@ -20,7 +20,7 @@ var sendJsonResponse = function (res, json) {
 //Send error response
 var sendErrorResponse = function (res, err) {
     try {
-        logger.trace('starting sendErrorResponse');
+        logger.trace('entering sendErrorResponse');
         res.send(err);
     } catch (err) {
         logger.error('rendering json response failed with error:' + err);
@@ -33,44 +33,48 @@ var sendErrorResponse = function (res, err) {
 //Get all members
 router.get('/', function (req, res, next) {
     try {
+        logger.trace('entering get all members');
         var promise = Members.fetchAllMembers();
         promise.then(json => sendJsonResponse(res, json), err => sendErrorResponse(res, err));
     } catch (err) {
         console.log('rendering json response failed with error:' + err);
     } finally{
-        logger.trace('leaving sendErrorResponse');
+        logger.trace('leaving get all members');
     }
 });
 
 //Get a single member by ID
 router.get('/:memberid', function (req, res, next) {
     try {
+        logger.trace('entering get member by id');
         var memberId = req.params.memberid;
         var promise = Members.fetchMemberById(memberId);
         promise.then(json => sendJsonResponse(res, json), err => sendErrorResponse(res, err));
     } catch (err) {
         console.log('rendering json response failed with error:' + err);
     } finally{
-        logger.trace('leaving sendErrorResponse');
+        logger.trace('leaving get member by id');
     }
 });
 
 //Get members by first name and last name
 router.get('/:firstName/:lastName', function (req, res, next) {
     try {
+        logger.trace('entering get member by name');
         var filterObject = { firstName: req.params.firstName, lastName: req.params.lastName }
         var promise = Members.filterMembers(filterObject);
         promise.then(json => sendJsonResponse(res, json), err => sendErrorResponse(res, err));
     } catch (err) {
         console.log('rendering json response failed with error:' + err);
     } finally{
-        logger.trace('leaving sendErrorResponse');
+        logger.trace('leaving get member by name');
     }
 });
 
 //Add a single member
 router.post('/', function (memberReq, res) {
     try {
+        logger.trace('entering post');
         var promise = Members.add(memberReq);
         promise.then(json => sendJsonResponse(res, json), err => sendErrorResponse(res, err));
     } catch (err) {
