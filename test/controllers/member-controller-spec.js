0f15571c0763fe335e183564ec8ad8347e79f72f
vinodm1986 <vinodm1986@gmail.com>
2016-04-23 19:04:00 -0400

    added mocha,chai and blanket to track test coverage

---
+++ test/controllers/member-controller-spec.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+
+describe('Get all members',function(){
+    it('returns all members',function(){
+        expect('hello').to.equal('hello');
+    })
+    it('fails')
+});
