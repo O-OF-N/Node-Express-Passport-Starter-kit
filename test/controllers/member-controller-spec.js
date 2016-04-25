22840c524339a38fe1563a3c75494118ac7e2d97
vinodm1986 <vinodm1986@gmail.com>
2016-04-25 15:05:48 -0400

    added test cases

--- test/controllers/member-controller-spec.js
+++ test/controllers/member-controller-spec.js
@@ -1,18 +1,28 @@
 var chai = require('chai');
 var expect = chai.expect;
 var sinon = require('sinon');
-sinonChai = require('sinon-chai');
+var sinonChai = require('sinon-chai');
 var Q = require("q");
 chai.use(sinonChai);
 
 var controller = require('../../app/controllers/member-controller');
 var model = require('../../app/models/member');
 var req,res,next = {};
+
 beforeEach(function () {
     res = {
-      json: sinon.spy()
+      json: sinon.spy(),
+      send: sinon.spy()
     };
+    sinon.spy(controller,'sendJsonResponse');
+    sinon.spy(controller,'sendErrorResponse');
   });
+  
+afterEach(function () {
+    res.send.reset();
+    controller.sendJsonResponse.restore();
+    controller.sendErrorResponse.restore();
+});
 
 describe('Get all members',function(){
     it('returns all members',function(done){
@@ -26,5 +36,25 @@ describe('Get all members',function(){
            done();  
         });
     });
-    it('fails')
+    it('model throws an error', function(done){
+       var err = {message: "failed to get response"};
+       var modelStub = sinon.stub(model,'fetchAllMembers').throws(err);
+       controller.getAllMembers(req,res,next);
+       process.nextTick(function(){
+           expect(res.send).to.have.been.calledWith(err); 
+           modelStub.restore();        
+           done();  
+        });  
+    });
+    it('model returns an error response from the db',function(){
+        const response = {message:'failed to fetch from db'};
+        var modelStub = sinon.stub(model,'fetchAllMembers').
+            returns(Q.resolve(response)); //returns a Q resolved response.
+        controller.getAllMembers(req,res,next);    
+        process.nextTick(function(){
+           expect(res.json).to.have.been.calledWith(response); 
+           modelStub.restore();        
+           done();  
+        });
+    });
 });
