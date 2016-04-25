08c4c11e9718eaa38c424fd7d3857ffffb96d868
vinodm1986 <vinodm1986@gmail.com>
2016-04-25 17:04:16 -0400

    covered with test cases.

--- test/controllers/member-controller-spec.js
+++ test/controllers/member-controller-spec.js
@@ -159,3 +159,98 @@ describe('Get members by first and last name',function(){
         });
     });
 });
+
+describe('Add a single member',function(){
+    var modelStub = {},firstName= "first",lastName = "last";
+    
+    beforeEach(function () {
+        req = {
+            params:{firstName:firstName,lastName:lastName}
+        };
+        modelStub = sinon.stub(model,'add');
+    });
+    
+    afterEach(function () {           
+        modelStub.restore();        
+    });
+    
+    it('successfully adds a member',function(done){
+        const response = {name:'member1'};
+        modelStub.returns(Q.resolve(response));
+        controller.addMemeber(req,res,next);    
+        process.nextTick(function(){
+           expect(res.json).to.have.been.calledWith(response); 
+           expect(modelStub).to.have.callCount(1);   
+           done();  
+        });
+    });
+    it('fails when model throws an error', function(done){
+       var err = {message: "failed to get response"};
+       modelStub.throws(err);
+       controller.addMemeber(req,res,next);
+       process.nextTick(function(){
+           expect(res.send).to.have.been.calledWith(err);  
+           expect(modelStub).to.have.callCount(1); 
+           done();  
+        });  
+    });
+    it('fails when model returns an error response from the db',function(){
+        const response = {message:'failed to fetch from db'};
+        modelStub.returns(Q.resolve(response));
+        controller.addMemeber(req,res,next);    
+        process.nextTick(function(){
+           expect(res.json).to.have.been.calledWith(response);     
+           expect(modelStub).to.have.callCount(1);    
+           done();  
+        });
+    });
+    
+    it('fails when id is not blank')
+});
+
+describe('Update a single member',function(){
+    var modelStub = {},firstName= "first",lastName = "last",id = "10";
+    
+    beforeEach(function () {
+        req = {
+            params:{id:id, firstName:firstName,lastName:lastName}
+        };
+        modelStub = sinon.stub(model,'updateByID');
+    });
+    
+    afterEach(function () {           
+        modelStub.restore();        
+    });
+    
+    it('successfully updates a member',function(done){
+        const response = {name:'member1'};
+        modelStub.returns(Q.resolve(response));
+        controller.updateMember(req,res,next);    
+        process.nextTick(function(){
+           expect(res.json).to.have.been.calledWith(response); 
+           expect(modelStub).to.have.callCount(1);   
+           done();  
+        });
+    });
+    it('fails when model throws an error', function(done){
+       var err = {message: "failed to get response"};
+       modelStub.throws(err);
+       controller.updateMember(req,res,next);
+       process.nextTick(function(){
+           expect(res.send).to.have.been.calledWith(err);  
+           expect(modelStub).to.have.callCount(1); 
+           done();  
+        });  
+    });
+    it('fails when model returns an error response from the db',function(){
+        const response = {message:'failed to fetch from db'};
+        modelStub.returns(Q.resolve(response));
+        controller.updateMember(req,res,next);    
+        process.nextTick(function(){
+           expect(res.json).to.have.been.calledWith(response);     
+           expect(modelStub).to.have.callCount(1);    
+           done();  
+        });
+    });
+    it('fails when memberid is blank')
+});
