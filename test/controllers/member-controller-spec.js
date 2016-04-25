0764a00ea515fd6599dbc810cfbb35696c88a1fb
vinodm1986 <vinodm1986@gmail.com>
2016-04-25 16:41:36 -0400

    adding more test cases.

--- test/controllers/member-controller-spec.js
+++ test/controllers/member-controller-spec.js
@@ -7,21 +7,22 @@ chai.use(sinonChai);
 
 var controller = require('../../app/controllers/member-controller');
 var model = require('../../app/models/member');
-var req,res,next = {};
+var req = {},res = {},next = {};
+var jsonSpy = {},errorSpy = {};
 
 beforeEach(function () {
     res = {
         json: sinon.spy(),
         send: sinon.spy()
     };
-    sinon.spy(controller,'sendJsonResponse');
-    sinon.spy(controller,'sendErrorResponse');
+    jsonSpy = sinon.spy(controller,'sendJsonResponse');
+    errorSpy = sinon.spy(controller,'sendErrorResponse');
 });
   
 afterEach(function () {
     res.send.reset();
-    controller.sendJsonResponse.restore();
-    controller.sendErrorResponse.restore();
+    jsonSpy.restore();
+    errorSpy.restore();
 });
 
 describe('Get all members',function(){
@@ -35,30 +36,125 @@ describe('Get all members',function(){
         modelStub.restore();        
     });
     
-    it('returns all members',function(done){
+    it('successfully returns all members',function(done){
         const response = [{name:'member1'},{name:'member2'}];
         modelStub.returns(Q.resolve(response));
         controller.getAllMembers(req,res,next);    
         process.nextTick(function(){
-           expect(res.json).to.have.been.calledWith(response); 
+           expect(res.json).to.have.been.calledWith(response);
+           expect(modelStub).to.have.callCount(1); 
            done();  
         });
     });
-    it('model throws an error', function(done){
+    it('fails when model throws an error', function(done){
        var err = {message: "failed to get response"};
        modelStub.throws(err);
        controller.getAllMembers(req,res,next);
        process.nextTick(function(){
-           expect(res.send).to.have.been.calledWith(err);      
+           expect(res.send).to.have.been.calledWith(err);  
+           expect(modelStub).to.have.callCount(1);     
            done();  
         });  
     });
-    it('model returns an error response from the db',function(){
+    it('fails when model returns an error response from the db',function(){
         const response = {message:'failed to fetch from db'};
         modelStub.returns(Q.resolve(response));
         controller.getAllMembers(req,res,next);    
         process.nextTick(function(){
-           expect(res.json).to.have.been.calledWith(response);        
+           expect(res.json).to.have.been.calledWith(response);     
+           expect(modelStub).to.have.callCount(1);    
+           done();  
+        });
+    });
+});
+
+describe('Get a single member by ID',function(){
+    var modelStub = {},id= 10;
+    
+    beforeEach(function () {
+        req = {
+            params:{id:id}
+        };
+        modelStub = sinon.stub(model,'fetchMemberById');
+    });
+    
+    afterEach(function () {           
+        modelStub.restore();        
+    });
+    
+    it('successfully returns all members',function(done){
+        const response = {name:'member1'};
+        modelStub.returns(Q.resolve(response));
+        controller.getMemberById(req,res,next);    
+        process.nextTick(function(){
+           expect(res.json).to.have.been.calledWith(response); 
+           expect(modelStub).to.have.callCount(1);   
+           done();  
+        });
+    });
+    it('fails when model throws an error', function(done){
+       var err = {message: "failed to get response"};
+       modelStub.throws(err);
+       controller.getMemberById(req,res,next);
+       process.nextTick(function(){
+           expect(res.send).to.have.been.calledWith(err);  
+           expect(modelStub).to.have.callCount(1); 
+           done();  
+        });  
+    });
+    it('fails when model returns an error response from the db',function(){
+        const response = {message:'failed to fetch from db'};
+        modelStub.returns(Q.resolve(response));
+        controller.getMemberById(req,res,next);    
+        process.nextTick(function(){
+           expect(res.json).to.have.been.calledWith(response);     
+           expect(modelStub).to.have.callCount(1);    
+           done();  
+        });
+    });
+});
+
+describe('Get members by first and last name',function(){
+    var modelStub = {},firstName= "first",lastName = "last";
+    
+    beforeEach(function () {
+        req = {
+            params:{firstName:firstName,lastName:lastName}
+        };
+        modelStub = sinon.stub(model,'filterMembers');
+    });
+    
+    afterEach(function () {           
+        modelStub.restore();        
+    });
+    
+    it('successfully returns all members',function(done){
+        const response = [{name:'member1'},{name:'member2'}];
+        modelStub.returns(Q.resolve(response));
+        controller.getMemberByName(req,res,next);    
+        process.nextTick(function(){
+           expect(res.json).to.have.been.calledWith(response); 
+           expect(modelStub).to.have.callCount(1);   
+           done();  
+        });
+    });
+    it('fails when model throws an error', function(done){
+       var err = {message: "failed to get response"};
+       modelStub.throws(err);
+       controller.getMemberByName(req,res,next);
+       process.nextTick(function(){
+           expect(res.send).to.have.been.calledWith(err);  
+           expect(modelStub).to.have.callCount(1); 
+           done();  
+        });  
+    });
+    it('fails when model returns an error response from the db',function(){
+        const response = {message:'failed to fetch from db'};
+        modelStub.returns(Q.resolve(response));
+        controller.getMemberByName(req,res,next);    
+        process.nextTick(function(){
+           expect(res.json).to.have.been.calledWith(response);     
+           expect(modelStub).to.have.callCount(1);    
            done();  
         });
     });
